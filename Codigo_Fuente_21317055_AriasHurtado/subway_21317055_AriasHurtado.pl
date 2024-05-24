085b40e7e7bcedab3a9bb4189f3dc0ad6ac27ecd
Jordan Arias <jordan.arias@usach.cl>
2024-05-23 23:41:51 -0400

    añadido subwayToString (por corregir), y añadido verificador de consistencia de linea (por corregir), ademas de mini test script en subway

--- Codigo_Fuente_21317055_AriasHurtado/subway_21317055_AriasHurtado.pl
+++ Codigo_Fuente_21317055_AriasHurtado/subway_21317055_AriasHurtado.pl
@@ -1,4 +1,4 @@
-:-module(subway_21317055_AriasHurtado, [subway/3, isTrains/1, subwayAddTrain/3, isLines/1, subwayAddLine/3, isDrivers/1, subwayAddDriver/3]).
+:-module(subway_21317055_AriasHurtado, [subway/3, isTrains/1, subwayAddTrain/3, isLines/1, subwayAddLine/3, isDrivers/1, subwayAddDriver/3,subwayToString/2]).
 :-use_module(train_21317055_AriasHurtado,[ isTrain/1, train/6]).
 :-use_module(line_21317055_AriasHurtado,[line/5, isLine/1]).
 :-use_module(driver_21317055_AriasHurtado,[driver/4, isDriver/1]).
@@ -41,17 +41,46 @@ subwayAddDriver(SubwayIn, Drivers, SubwayOut):-
     SubwayOut = [SubwayIn, Drivers].
 
 
+subwayToString([], _).
+subwayToString([H|T], SubwayStr):-
+    writeln(H),
+    subwayToString(T,SubwayStr).
+
 
 /*
+station( 1, "USACH", "c", 30, ST1),
+station( 2, "Estación Central", "c", 45, ST2),
+station( 3, "ULA", "r", 45, ST3),
+station( 4, "San Pablo", "t", 40, ST4).
+station( 5, "Los Dominicos", "t", 60, ST5).
+
+section(ST1, ST2, 2, 50, S0),
+section( ST2, ST3, 2.5, 55, S1),
+
+line( 0, "Línea 0", "UIC 60 ASCE", [S0,S1], L0),
+
+pcar( 0, 90, "NS-74", ct, PC0),
+pcar( 1, 100, "NS-74", tr, PC1),
+pcar( 2, 150, "NS-74", tr, PC2),
+pcar( 3, 90, "NS-74", ct, PC3),
+
+pcar( 0, 90, "NS-74", "ct", PC0),
+pcar( 1, 100, "NS-74", "tr", PC1),
+pcar( 2, 150, "NS-74", "tr", PC2),
+pcar( 3, 90, "NS-74", "ct", PC3),
+
+train( 1, "CAF", "UIC 60 ASCE", 70, [PC1, PC0, PC3, PC2], T1),
+
+driver( 0, "name0", "CAF", D0).
+
+
+
 subway( 0, "Metro Santiago", S0),
 
 subwayAddTrain( Sw0, [T1], Sw1),
-subwayAddTrain( Sw1, [T2, T3, T4], Sw2).
 
-subwayAddLine( Sw5, [L5], Sw6).
-subwayAddLine( Sw6, [L6, L7], Sw7).
+subwayAddLine( Sw1, [L0], Sw2),
 
-subwayAddDriver( Sw7, [D0], Sw8).
-subwayAddDriver( Sw8, [D1, D2, D3], Sw9).
+subwayAddDriver( Sw2, [D0], Sw3),
 
 */
